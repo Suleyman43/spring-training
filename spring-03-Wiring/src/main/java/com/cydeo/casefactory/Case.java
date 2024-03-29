fd3444c3653689783786500ec607d7d90db6587e
suleyman aydin <suleymanscience38@gmail.com>
2024-03-29 05:59:20 +0000

    PcConfig added1..

--- spring-03-Wiring/src/main/java/com/cydeo/casefactory/Case.java
+++ spring-03-Wiring/src/main/java/com/cydeo/casefactory/Case.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 @Setter
 public abstract class Case {
 
+
     private String model;
     private String manufacturer;
     private String powerSupply;
     private Dimensions dimensions;
 
     public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+
         this.model = model;
         this.manufacturer = manufacturer;
         this.powerSupply = powerSupply;
