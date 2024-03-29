64c117749a00598ec0206888802d6a75e181e797
suleyman aydin <suleymanscience38@gmail.com>
2024-03-29 07:20:15 +0000

    PcConfig added1..

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -10,13 +10,17 @@ import org.springframework.stereotype.Component;
 
 @Getter
 @Component
+
 public class PC {
 
     private Case theCase;
+
     private Monitor monitor;
+
     private Motherboard motherboard;
 
 
+    @Autowired
     public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
         this.theCase = theCase;
         this.monitor = monitor;
