4c96985c4ffdec7f1ee6c82648bf807e0e84dce2
suleyman aydin <suleymanscience38@gmail.com>
2024-04-03 18:37:20 +0100

    added_4th april

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -2,13 +2,14 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CydeoApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(CydeoApplication.class, args);
+        ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
     }
 
 }
