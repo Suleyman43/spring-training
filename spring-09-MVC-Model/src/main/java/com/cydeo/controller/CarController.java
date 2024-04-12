9b6d7b7039cba06ca9db2daaa8afad6acb690891
suleyman aydin <suleymanscience38@gmail.com>
2024-04-13 00:07:49 +0100

    added_13th april

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -9,7 +9,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CarController {
 
-    public String carInfo(){
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make, Model model){
+
+        model.addAttribute("make",make);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value="make",required = false,defaultValue = "KIA") String make, Model model){
+
+        model.addAttribute("make",make);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info3")
+    public String carInfo3(@RequestParam String make, @RequestParam int year, Model model){ //localhost:8080/info3?make=Honda&year=2015
+
+
+        model.addAttribute("make",make);
+        model.addAttribute("year",year);
+        return "car/car-info";
+    }
+
+    //localhost:8080/info/honda/2015
+    @RequestMapping("/info/{make}/{year}")
+    public String getCarInfo(@PathVariable String make, @PathVariable int year) {
+
+        System.out.println(make);
+        System.out.println(year);
 
         return "car/car-info";
     }
