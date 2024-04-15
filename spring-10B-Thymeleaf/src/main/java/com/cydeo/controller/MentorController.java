420383ca181706979d2122f7cfafe18acae46d81
suleyman aydin <suleymanscience38@gmail.com>
2024-04-16 00:40:04 +0100

    added_16 th april

--- spring-10B-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10B-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,16 +1,23 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
+@RequestMapping("/mentor")
 public class MentorController {
 
 
     //@RequestMapping("/register")
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model){
 
-        return "mentor/register";
+        model.addAttribute("mentor",new Mentor());
+        return "mentor/mentor-register";
     }
 
 
